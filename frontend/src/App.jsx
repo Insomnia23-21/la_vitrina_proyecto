abc3b8a649c2c3d2c88d00bed62ea9a6f42b7ac8
Insomnia23-21 <danielmedinageek@gmail.com>
2024-12-18 02:25:59 -0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ const App = () => {
       <CartProvider>
         <Router>
           <NavBar />
+          <div className="container-fluid">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
@@ -30,6 +31,7 @@ const App = () => {
             <Route path="/product/:id" element={<ProductDetails />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
+          </div>
           <Footer />
         </Router>
       </CartProvider>
