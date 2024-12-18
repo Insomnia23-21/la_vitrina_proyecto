53dac58b87c057582c7d53733447b381d928ea12
Insomnia23-21 <danielmedinageek@gmail.com>
2024-12-18 03:12:35 -0300

    .

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../context/ContextProvider";
 import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 
