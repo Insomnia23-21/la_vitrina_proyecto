bd5f0af1ca63cc86a6de29a85b74a451d2ea4566
Insomnia23-21 <danielmedinageek@gmail.com>
2024-12-18 01:50:05 -0300

    .

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -14,6 +14,20 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Obtener un producto por ID
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query("SELECT * FROM products WHERE id = $1", [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // Crear una nueva publicación (protegida por autenticación)
 router.post("/create", protect, async (req, res) => {
   const { title, description, image } = req.body;
